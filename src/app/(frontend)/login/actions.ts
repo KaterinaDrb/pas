2d56f48ad770d16ad03d518d3ddf785878cb2806
Vladislav Mukhin <vladisl.mukhin@gmail.com>
2026-02-13 13:30:47 +0300

    🐞fix(auth): set cookie

--- src/app/(frontend)/login/actions.ts
+++ src/app/(frontend)/login/actions.ts
@@ -2,16 +2,20 @@
 import { redirect } from 'next/navigation';
 import api from '@/api';
 import { AxiosError } from 'axios';
+import { cookies } from 'next/headers';
+import { type User } from '@/payload-types';
 
 export async function loginAction(
   state: { errors: { message: string }[] } | undefined,
   formData: FormData
 ) {
   try {
-    await api.post('/users/login', {
+    const { data } = await api.post<User & { token: string }>('/users/login', {
       email: formData.get('email'),
       password: formData.get('password'),
     });
+    const cookie = await cookies();
+    cookie.set('payload-token', data.token);
   } catch (error) {
     if (error instanceof AxiosError) {
       return { errors: error.response?.data.errors as { message: string }[] };
